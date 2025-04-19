73a14b91f58772630323a8a85a635512ee060cc8
minhyeok <133848892+newrang@users.noreply.github.com>
2025-04-19 19:32:28 +0900

    Update judges.c

--- judges.c
+++ judges.c
@@ -4,37 +4,39 @@
 
 typedef struct {
     char name[30];
-    char gender[4];
+    char gender[10];
     char enter[50];
     char job[30];
-    char pro[10];
+    char pro[30];
     char mail[70];
     char number[80];
 } Judge;
 
+// 문자열 입력 함수: 공백 포함해서 입력받고 줄바꿈 제거
 void input(char* prompt, char* buffer, int size) {
     printf("%s", prompt);
     fgets(buffer, size, stdin);
-    buffer[strcspn(buffer, "\n")] = '\0'; // 개행문자 제거
+    buffer[strcspn(buffer, "\n")] = '\0'; // 개행 문자 제거
 }
 
 int main(void) {
-    Judge judges[5];
-    char yn;
+    Judge judges[5]; // 5명의 심사위원 정보 저장
+    char yn; // Y/N 입력
     int i = 0;
 
-    while (i < 5) {
-        printf("####################################\n");
-        printf("#      심사위원 명단 데이터 입력       #\n");
-        printf("####################################\n");
-        printf("> 참여 프로젝트: 밀리웨이즈\n");
-        printf("> 심사 총 인원: 5\n");
-        printf("> 선발 멤버 수: 4\n");
-        printf("++++++++++++++++++++++++++++++++++++\n");
-        printf("5명의 심사위원 정보 입력을 시작합니다.\n");
-        printf("++++++++++++++++++++++++++++++++++++\n");
+    // 심사위원 정보 입력
+    printf("####################################\n");
+    printf("#      심사위원 명단 데이터 입력       #\n");
+    printf("####################################\n");
+    printf("> 참여 프로젝트: 밀리웨이즈\n");
+    printf("> 심사 총 인원: 5\n");
+    printf("> 선발 멤버 수: 4\n");
+    printf("++++++++++++++++++++++++++++++++++++\n");
+    printf("5명의 심사위원 정보 입력을 시작합니다.\n");
+    printf("++++++++++++++++++++++++++++++++++++\n");
 
-        printf("[%d번 심사위원]\n", i + 1);
+    while (i < 5) {
+        printf("\n[%d번 심사위원]\n", i + 1);
 
         input("이름: ", judges[i].name, sizeof(judges[i].name));
         input("성별: ", judges[i].gender, sizeof(judges[i].gender));
@@ -45,18 +47,19 @@ int main(void) {
         input("전화번호: ", judges[i].number, sizeof(judges[i].number));
 
         i++;
-        printf("\n");
     }
 
-    printf("[PROJECT] 심사위원 정보를 확인할까요? Y/N: ");
+    // 사용자에게 출력 여부 확인
+    printf("\n[PROJECT] 심사위원 정보를 확인할까요? Y/N: ");
     scanf(" %c", &yn);
-    getchar(); // 남은 개행 제거
+    getchar(); // 버퍼에 남은 개행 제거
 
     if (yn == 'N' || yn == 'n') {
         printf("프로그램을 종료합니다.\n");
         return 0;
     } else if (yn == 'Y' || yn == 'y') {
-        printf("####################################\n");
+        // 출력
+        printf("\n####################################\n");
         printf("#        심사위원 데이터 출력        #\n");
         printf("####################################\n");
 
